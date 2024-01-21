960ee0a7c6f23023ba4ec936fafbb105ea4d4c05
Piotr <114903054+elevenhsoft@users.noreply.github.com>
2024-01-21 12:27:44 +0100

    add missing dependency

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -10,6 +10,7 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim", -- required
 			"sindrets/diffview.nvim", -- optional - Diff integration
+			"nvim-tree/nvim-web-devicons",
 
 			-- Only one of these is needed, not both.
 			"nvim-telescope/telescope.nvim", -- optional
