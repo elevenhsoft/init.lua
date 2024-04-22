4d65d45eb6d7e0ee97547b6916335bcac176b365
Piotr <114903054+elevenhsoft@users.noreply.github.com>
2024-04-22 15:57:06 +0200

    formatting

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,28 +3,26 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-	'mbbill/undotree',
+  'mbbill/undotree',
+  {
+    'NeogitOrg/neogit',
+    branch = 'nightly',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+      'nvim-tree/nvim-web-devicons',
 
-	{
-		"NeogitOrg/neogit",
-		branch = "nightly",
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
-			"sindrets/diffview.nvim", -- optional - Diff integration
-			"nvim-tree/nvim-web-devicons",
+      -- Only one of these is needed, not both.
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+    },
+    config = true,
+  },
 
-			-- Only one of these is needed, not both.
-			"nvim-telescope/telescope.nvim", -- optional
-			"ibhagwan/fzf-lua", -- optional
-		},
-		config = true
-	},
-
-	{
-		'ThePrimeagen/harpoon',
-		dependencies = {
-			'nvim-lua/plenary.nvim'
-		}
-	},
-
+  {
+    'ThePrimeagen/harpoon',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
 }
