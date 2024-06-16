292e896ec2a4de3aaeeddd900b45e7d9ae82f760
Piotr <114903054+elevenhsoft@users.noreply.github.com>
2024-06-16 13:10:36 +0200

    update

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
+vim.g.python3_host_prog = '/usr/bin/python3'
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
