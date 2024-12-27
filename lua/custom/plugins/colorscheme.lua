c3e6f18c1f387a506eeef2914ff0baf0a0bb123f
Piotr <114903054+elevenhsoft@users.noreply.github.com>
2024-12-27 17:25:15 +0100

    set lunaperche as colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    'RRethy/base16-nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'base16-black-metal-venom'
-    end,
-  },
+  -- {
+  --   'RRethy/base16-nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'base16-black-metal-venom'
+  --   end,
+  -- },
 }
